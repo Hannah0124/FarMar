49a3cd683fc447788d6ac75208b1597cb06949ff
Hannah <hannahjoo24@gmail.com>
2020-02-29 17:45:07 -0800

    Added some validation in Market#id

--- lib/market.rb
+++ lib/market.rb
@@ -1,10 +1,20 @@
 module FarMar 
   class Market 
-    attr_reader :id 
+    attr_reader :id, :name, :address, :city, :country, :state, :zip 
     
     def initialize(id, name, address, city, country, state, zip)
-      @id = id
 
+      unless (id.instance_of? Integer) && (id > 0)
+        raise ArgumentError.new("ID must be a positive integer (got #{id})")
+      end 
+
+      @id = id
+      @name = name 
+      @address = address 
+      @city = city 
+      @country = country 
+      @state = state 
+      @zip = zip
     end 
   end 
 end 
--- test/market_test.rb
+++ test/market_test.rb
@@ -6,15 +6,51 @@ describe "Market" do
       market = FarMar::Market.new(1, "People's Co-op Farmers Market", "30th and Burnside", "Portland","Multnomah", "Oregon", "97202"
       )
 
-      market.must_be_kind_of FarMar::Market
+      expect(market).must_be_kind_of FarMar::Market
     end 
 
     it "Keeps track of ID" do 
       id = 1337
       market = FarMar::Market.new(id, "People's Co-op Farmers Market", "30th and Burnside", "Portland","Multnomah", "Oregon", "97202"
       ) 
-      market.must_respond_to :id 
-      market.id.must_equal id
+      expect(market).must_respond_to :id 
+      expect(market.id).must_equal id
+    end 
+
+    it "Requires a integer ID" do 
+      proc {
+        FarMar::Market.new("not an integer", "test", "test", "test", "test", "test", "test")
+      }.must_raise ArgumentError
+    end 
+
+    it "Requires a positive ID" do 
+      proc {
+        FarMar::Market.new(-10, "test", "test", "test", "test", "test", "test")
+      }.must_raise ArgumentError
+    end 
+
+    it "Keeps track of country" do 
+      country = "USA"
+      market = FarMar::Market.new(1, "People's Co-op Farmers Market", "30th and Burnside", "Portland",country, "Oregon", "97202"
+      ) 
+      expect(market).must_respond_to :country
+      expect(market.country).must_equal country
+    end 
+
+    it "Keeps track of ID" do 
+      state = "test"
+      market = FarMar::Market.new(1, "People's Co-op Farmers Market", "30th and Burnside", "Portland","Multnomah", state, "97202"
+      ) 
+      expect(market).must_respond_to :state
+      expect(market.state).must_equal state
+    end 
+
+    it "Keeps track of ID" do 
+      zip = "98107"
+      market = FarMar::Market.new(1, "People's Co-op Farmers Market", "30th and Burnside", "Portland","Multnomah", "Oregon", zip
+      ) 
+      expect(market).must_respond_to :zip 
+      expect(market.zip).must_equal zip
     end 
   end 
 end 
